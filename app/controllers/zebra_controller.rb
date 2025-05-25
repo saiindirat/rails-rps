2f9c7194ee349ae46aba3ffd8a1d365b8bd6b416
saiindirat <tholusaiindira@gmail.com>
2025-05-25 10:19:12 +0000

    my final commit

--- app/controllers/zebra_controller.rb
+++ app/controllers/zebra_controller.rb
@@ -1,7 +1,20 @@
 class ZebraController < ApplicationController
   def giraffe
     @random_move = ["rock", "paper", "scissors"].sample
-
     render({ :template => "game_templates/play_rock" })
+  end
+
+  def elephant
+    @random_move = ["rock", "paper", "scissors"].sample
+    render({ :template => "game_templates/play_paper" })
+  end
+
+  def lion
+    @random_move = ["rock", "paper", "scissors"].sample
+    render({ :template => "game_templates/play_scissors" })
+  end
+
+  def rules
+    render({ :template => "game_templates/home" })
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   get("/rock", { :controller => "zebra", :action => "giraffe" })
   get("/paper", { :controller => "zebra", :action => "elephant" })
   get("/scissors", { :controller => "zebra", :action => "lion" })
+  get("/", { :controller => "zebra", :action => "rules" })
 end
